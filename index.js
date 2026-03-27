7b6666c45812d09ff1e09544d21c525150f20538
Jonathan <jdelar94@outlook.com>
2026-03-27 11:05:05 +0100

    delete update

--- index.js
+++ index.js
@@ -2,14 +2,10 @@
 
 import { argv } from "node:process";
 import { check } from "./src/check.js";
-import { update } from "./src/update.js";
 
 switch (argv[2]) {
     case "check":
         await check();
-        break;
-    case "update":
-        await update();
         break;
     default:
         console.info("Command not found...");
